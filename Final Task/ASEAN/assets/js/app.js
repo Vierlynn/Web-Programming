ef39bb27c71325b03c15e2cb1d770f061ed77ec0
Nanda Hakiki <37254184+Vierlynn@users.noreply.github.com>
2020-05-18 16:56:14 +0700

    Add files via upload

---
+++ Final Task/ASEAN/assets/js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('#tbProduk').DataTable({
+        "columns": [
+            {"width": "5%"},
+            {"width": "10%"},
+            null,
+            null,
+            {"width": "11%"},
+            {"width": "5%"}
+        ]
+    });
+});
+
+function del($url){
+    swal({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        calcelButtonColor: '$d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) =>{
+        if (result.value){
+            location.href = '/' + $url;
+        }
+    })
+}
